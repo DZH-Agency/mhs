462b5608105d012d00af5521842fb79bd1983d90
Sundwell <lyamtsev.s2017@gmail.com>
2022-04-17 22:56:08 +0300

    Add RankingTable

---
+++ pages/test.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <portal-target name="app" />
+    <top-nav-bar/>
+    <div class="table-wrapper">
+      <div class="container">
+        <div class="table">
+          <div class="table-top">
+            <h1 class="table-top__heading">Ranking <span class="text-purple-gradient">Table</span></h1>
+            <p class="table-top__subtext">Use the below table to find the ranking of Mad Hares.</p>
+          </div>
+          <div class="table-search">
+            <input type="text" placeholder="Search" class="table-search__input">
+            <button class="table-search__button"/>
+          </div>
+          <div class="table-sort">
+            <div class="table-sort__item active">Sort By Rarity</div>
+            <div class="table-sort__item">Sort By ID</div>
+          </div>
+          <div class="table-nfts">
+            <div class="table-nfts-card" v-for="_ in Array(12)">
+              <div class="table-nfts-card-img">
+                <img src="https://placehold.jp/3d4070/ffffff/150x150.png" alt="" class="table-nfts-card-img__img">
+              </div>
+              <div class="table-nfts-card__name">
+                Mad Hares #7912
+              </div>
+              <div class="table-nfts-card__score">
+                Score: 16000
+              </div>
+            </div>
+          </div>
+          <div class="table-paginator">
+            <button :disabled="currentPageNumber === 1" @click="currentPageNumber--" class="table-paginator__arrow prev">
+              <img alt="arrow" src="/RankingTable/left-arrow.svg"/>
+            </button>
+            <button
+              class="table-paginator__page"
+              v-for="page in availablePages"
+              :key="page.number"
+              :class="{active: currentPageNumber === page.number}"
+              @click="currentPageNumber = page.number || 1"
+            >
+              {{ page.number || '...' }}
+            </button>
+            <button :disabled="currentPageNumber === pages.length" @click="currentPageNumber++" class="table-paginator__arrow next">
+              <img alt="arrow" src="/RankingTable/left-arrow.svg"/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <the-footer/>
+  </div>
+</template>
+
+<script>
+import {fill} from 'lodash'
+
+export default {
+  name: 'test',
+  data() {
+    return {
+      totalPagesCount: 32,
+      startPagesCount: 2,
+      endPagesCount: 1,
+      middlePagesOffset: 1,
+      currentPageNumber: 1
+    }
+  },
+  computed: {
+    pages() {
+      return Array.from(Array(this.totalPagesCount)).map((_, index) => index + 1)
+    },
+    availablePages() {
+      const startPages = this.pages.slice(0, this.startPagesCount)
+      const endPages = this.pages.slice(-this.endPagesCount).filter(pageNumber => !startPages.includes(pageNumber))
+      
+      const middlePages = this._getMiddlePages(startPages, endPages)
+      
+      const rawPages = [...startPages, ...middlePages, ...endPages]
+      
+      return this._processPages(rawPages)
+    }
+  },
+  methods: {
+    _getMiddlePages(startPages, endPages) {
+      const middlePagesRawTemplate = fill(Array(this.middlePagesOffset * 2 + 1), null)
+      const middlePagesTemplate = middlePagesRawTemplate.map((pageNumber, index) => {
+        return this.currentPageNumber - this.middlePagesOffset + index
+      })
+    
+      const middlePages = middlePagesTemplate.filter(pageNumber => pageNumber > 0 && pageNumber < this.totalPagesCount)
+    
+      const uniqueMiddlePages = middlePages.filter(pageNumber => !startPages.includes(pageNumber) && !endPages.includes(pageNumber))
+    
+      if (uniqueMiddlePages.length === 0) {
+        return endPages.length ? ['...'] : []
+      }
+    
+      if (uniqueMiddlePages[0] !== startPages[startPages.length - 1] + 1) {
+        uniqueMiddlePages.splice(0, 0, '...')
+      }
+    
+      if (uniqueMiddlePages[uniqueMiddlePages.length - 1] !== endPages[0] - 1) {
+        uniqueMiddlePages.push('...')
+      }
+    
+      return uniqueMiddlePages
+    },
+    _processPages(pages) {
+      return pages.map(pageItem => {
+        const pageData = {
+          number: null,
+          isDummy: true
+        }
+      
+        if (typeof pageItem === 'number') {
+          pageData.number = pageItem
+          pageData.isDummy = false
+        }
+      
+        return pageData
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/scss/RankingTable.scss";
+</style>
