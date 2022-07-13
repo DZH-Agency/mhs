4b1bda186217b2e2141ef2a31f1354437d7cca06
DmitryStokozOfficial <dmitry.stokoz@gmail.com>
2022-07-13 14:30:34 +0300

    Add hidden-nft

---
+++ pages/hidden-nft.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hidden-nft-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'hidden-nft'
+}
+</script>
+
+<style scoped lang="scss">
+.hidden-nft-wrapper {
+  height: 100vh;
+  background: url("static/HiddenNFT/desktop.png") no-repeat center / 100% 100%;
+  
+  @media (max-width: 700px) {
+    background: url("static/HiddenNFT/mobile.png") no-repeat center bottom / 100% 100%;
+  }
+}
+</style>
