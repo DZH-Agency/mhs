4cb5bf521f21906833c21ee8e4f76bc4da5438e8
serhii.liamtsev <serhiiliamtsev@gmail.com>
2023-03-14 22:09:01 +0200

    Guide page

---
+++ pages/mutants/guide.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper guide-wrapper">
+    <img
+      src="/img/faq/faq-cloud.png"
+      alt="cloud"
+      class="guide__cloud"
+    >
+    <div class="container guide-container">
+      <div class="guide">
+        <img
+          src="/img/bottom-img.png"
+          alt="preview"
+          class="guide__bottom-img"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "guide"
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/pages/mutants/guide.scss";
+</style>
