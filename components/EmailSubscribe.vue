e1031a7c76e431a911f51e2758880ce9ced9eeb7
SerhiiLiamtsev <lyamtsev.s2017@gmail.com>
2023-04-23 16:45:53 +0300

    Added email subscribe

--- components/EmailSubscribe.vue
+++ components/EmailSubscribe.vue
@@ -1,13 +1,71 @@
 <template>
-$END$
+  <div class="wrapper es-wrapper">
+    <div class="container">
+      <section class="es">
+        <div class="es-hat">
+          <img src="/img/email-bell.png" alt="email-bell.png" class="es-hat__email-bell-icon">
+        </div>
+        <h2 class="es__title">
+          Don't miss out on exclusive&nbsp;
+          <strong class="text-purple-gradient">updates & surprises:</strong>
+        </h2>
+        <template v-if="!isSubscribed">
+          <section class="es-input">
+            <input v-model="email" class="es-input__sing-up-input" placeholder="email@example.com" type="text">
+<!--            TODO remove false and parenthesis-->
+            <button :disabled="false && (!isEmailValid || !email)" class="btn es-input__sing-up-button" @click="subscribe">Sign Up</button>
+          </section>
+          <section class="es-errors">
+            <span class="es-errors__error" v-if="!isEmailValid">Email is invalid</span>
+            <span class="es-errors__error" v-if="error">{{ error }}</span>
+          </section>
+        </template>
+
+        <section class="es-success" v-else>
+          <button class="btn es-success__back-button" @click="goBack">Back</button>
+          <button class="btn es-success__resent-button">Resend email</button>
+          <p class="es-success__text">Check verifying link at your email and press to verify</p>
+          <button class="btn es-success__have-button">I have verified email</button>
+        </section>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "EmailSubscribe"
+  name: "EmailSubscribe",
+  data() {
+    return {
+      isSubscribed: true,
+      error: '',
+      email: ''
+    }
+  },
+  computed: {
+    isEmailValid() {
+      if (!this.email) {
+        return true
+      }
+
+      return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(this.email)
+    }
+  },
+  methods: {
+    async subscribe() {
+      // TODO add real logic
+      // this.error = this.error ? '' : 'Something went wrong'
+      this.isSubscribed = true
+      this.email = ''
+    },
+    goBack() {
+      this.isSubscribed = false
+      this.error = ''
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "~@/assets/scss/components/EmailSubscribe.scss";
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <portal-target name="app"/>
     <top-nav-bar/>
+    <email-subscribe />
     <meet-mad-society/>
     <exclusively-on :timer-date="timerDate"/>
     <div id="story">
@@ -46,34 +47,34 @@ export default {
     const targetDate = new Date(Date.UTC(2022, 3, 15, 13))
     targetDate.setHours(targetDate.getHours() + 2 + new Date().getTimezoneOffset() / 60)
     this.$options.targetDate = targetDate
-  
+
     setInterval(() => {
       const today = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + new Date().getTimezoneOffset() / 60 * 1000)
       today.setHours(today.getHours() + 2)
-      
+
       let diffInSeconds = Math.floor((this.$options.targetDate - today) / 1000);
       if (diffInSeconds <= 0) {
         return
       }
-    
+
       const days = Math.floor(diffInSeconds / 86400);
       diffInSeconds -= days * 86400;
-    
+
       // calculate hours
       const hours = Math.floor(diffInSeconds / 3600) % 24;
       diffInSeconds -= hours * 3600;
-    
+
       // calculate minutes
       const minutes = Math.floor(diffInSeconds / 60) % 60;
       diffInSeconds -= minutes * 60;
-    
+
       this.timerDate = {
         days: String(days).padStart(2, '0'),
         hours: String(hours).padStart(2, '0'),
         minutes: String(minutes).padStart(2, '0'),
         seconds: String(diffInSeconds).padStart(2, '0')
       }
-    
+
     }, 1000)
   }
 }
