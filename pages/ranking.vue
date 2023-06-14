e34bea8cc5d4d197f4a1372ebbc9f7c2bf62fd6a
serhii.liamtsev <lyamtsev.s2017@gmail.com>
2023-06-14 03:01:47 +0300

    Update ranking page

--- pages/ranking.vue
+++ pages/ranking.vue
@@ -13,23 +13,28 @@
             <input v-model="search" type="text" placeholder="Search" class="table-search__input">
             <button class="table-search__button"/>
           </div>
+          <div class="table-filter">
+            <button class="table-filter__item" @click="collection = 'hares'" :class="{active: collection === 'hares'}">Mad Hares</button>
+            <button class="table-filter__item" @click="collection = 'mutants'" :class="{active: collection === 'mutants'}"> Mad Mutants</button>
+            <button class="table-filter__item" @click="collection = 'genqbabies'" :class="{active: collection === 'genqbabies'}">Gen-Q Elite Babies</button>
+          </div>
           <div class="table-sort">
             <div class="table-sort__item" :class="{active: sort === 'rarity'}" @click="sort = 'rarity'">Sort By Rarity</div>
             <div class="table-sort__item" :class="{active: sort === 'id'}" @click="sort = 'id'">Sort By ID</div>
           </div>
           <div class="table-nfts">
-            <div class="table-nfts-card" v-for="item in items" :key="item.asset_id">
+            <div class="table-nfts-card" v-for="item in items" :key="`${collection}-${item.asset_id || item.ID}`">
               <a :href="getNftLink(item)" target="_blank" class="table-nfts-card-img">
-                <img :src="item.hare_img" alt="" class="table-nfts-card-img__img">
+                <img :src="item[imgKey]" alt="" class="table-nfts-card-img__img">
               </a>
               <a target="_blank" :href="getNftLink(item)" class="table-nfts-card__name">
-                Mad Hare #{{ item.mutant_id }}
+                {{ currentCollectionName }} #{{ item.mutant_id || item.ID }}
               </a>
               <div class="table-nfts-card__score">
-                Score: {{ Number(item.score).toFixed(4) }}
+                Score: {{ Number(item.score || item.Score).toFixed(4) }}
               </div>
               <div class="table-nfts-card__rank">
-                Rank: <strong>{{ Number(item.rank) }}</strong> / 10000
+                Rank: <strong>{{ Number(item.ranking) }}</strong> / 10000
               </div>
             </div>
           </div>
@@ -72,9 +77,28 @@ export default {
       items: [],
       sort: 'rarity',
       search: '',
+      collection: 'hares',
     }
   },
   computed: {
+    imgKey() {
+      const keyByCollection = {
+        hares: 'hare_img',
+        mutants: 'img_url',
+        genqbabies: 'img',
+      }
+      
+      return keyByCollection[this.collection]
+    },
+    currentCollectionName() {
+      const nameByCollection = {
+        hares: 'Mad Hares',
+        mutants: 'Mad Mutants',
+        genqbabies: 'Gen-Q Baby',
+      }
+      
+      return nameByCollection[this.collection]
+    },
     pages() {
       return Array.from(Array(this.totalPagesCount)).map((_, index) => index + 1)
     },
@@ -90,13 +114,19 @@ export default {
     }
   },
   watch: {
+    collection() {
+      this.currentPageNumber = 1
+      this.loadItems()
+    },
     async search() {
+      this.currentPageNumber = 1
       await this.debouncedSearch()
     },
     currentPageNumber() {
       this.loadItems()
     },
     sort() {
+      this.currentPageNumber = 1
       this.loadItems()
     },
   },
@@ -108,7 +138,13 @@ export default {
       await this.loadItems()
     }, 300),
     getNftLink(item) {
-      return `https://crypto.com/nft/collection/41a371f626f43473ca087f0f36f06299?asset=${item.assetId}&edition=${item.defaultEditionId}&detail-page=MARKETPLACE`
+      const collectionIdByCollection = {
+        hares: 'faa3d8da88f9ee2f25267e895db71471',
+        mutants: '69d0601d6d4ecd0ea670835645d47b0d',
+        genqbabies: 'b8a462abcadfa08f1e678df15bbacc41'
+      }
+      const collectionId = collectionIdByCollection[this.collection]
+      return `https://crypto.com/nft/collection/${collectionId}?asset=${item.asset_id || item.ID}&edition=${item.defaultEditionId}&detail-page=MARKETPLACE`
     },
     async loadItems() {
       const { data } = await this.$axios.get('/search', {
@@ -116,7 +152,8 @@ export default {
           page: this.currentPageNumber,
           sort_by: this.sort,
           sort: this.sort === 'rarity' ? 'DESC' : 'ASC',
-          query: this.search
+          query: this.search,
+          collection: this.collection === 'hares' ? null : this.collection,
         }
       })
 
